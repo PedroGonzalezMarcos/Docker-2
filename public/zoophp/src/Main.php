4a84c4e98d88193e74fa5de7e58ff0c24a99cb2e
PedroGonzalezMarcos <not2pedro@gmail.com>
2023-05-19 16:37:24 +0200

    Holi

--- public/zoophp/src/Main.php
+++ public/zoophp/src/Main.php
@@ -39,6 +39,7 @@ echo "Especie: " . $especie . "\n";
 echo "Altura: " . $altura . "m.\n";
 echo "Espacio: " . $espacio . ".\n";
 echo "Frutal: " . ($frutal ? "Sí" : "No") . "\n";
+echo "the dolphin";
 
 $animal1 = new Animal("Luna", "Perro", 5);
 $animal1->setEdad(6);
